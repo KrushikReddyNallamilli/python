18673f730abf002bb7c60cc2fd29a4f18c0bc99b
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-06-30 10:47:39 +0530

    Create turtle program to create cicles.py

---
+++ turtle program to create cicles.py
@@ -0,0 +1,14 @@
+from turtle import*
+t=Turtle()
+t.color("green")
+for i in range(4):
+    t.circle(30)
+    t.left(30)
+    t.color("blue")
+    t.circle(30)
+    t.left(30)
+    t.color("red")
+    t.circle(30)
+    t.left(30)
+    t.color("green")
+done()
